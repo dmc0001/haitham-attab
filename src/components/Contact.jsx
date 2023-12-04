f06d144026548b2261fb777e1278504362e0a1f4
jeo <abdeldjalildh990@gmail.com>
2023-12-04 19:25:55 +0100

    last commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,22 +1,31 @@
 const Contact = () => {
   return (
-    <div id="contact" className="flex flex-col justify-center items-center mb-52">
+    <div
+      id="contact"
+      className="flex flex-col justify-center items-center mb-52"
+    >
       <div className="flex gap-3 items-baseline">
-        <span className="text-[#64FFDA] font-fira text-[20px] md:text-[24px]">04.</span>
-        <span className="text-[#fff] font-fira text-[26px] md:text-[32px]s">Wanna talk ?</span>
+        <span className="text-[#64FFDA] font-fira text-[20px] md:text-[24px]">
+          04.
+        </span>
+        <span className="text-[#fff] font-fira text-[26px] md:text-[32px]s">
+          Wanna talk ?
+        </span>
       </div>
       <p className="text-center text-[#CCD6F6] font-fira text-[20px] md:text-[24px] md:w-[782px]">
         <br />
         <br />
         <br /> I’m always looking for new opportunities and in various form:
-        Freelance, Part-time or Contract. <br /> <br /><br />
+        Freelance, Part-time or Contract. <br /> <br />
+        <br />
       </p>
-      <button
-  className="text-[#64FFDA] border-[0.5px] font-fira text-[14px] w-fit py-3 px-12 rounded-[5px] border-[#64FFDA] transition-all duration-300 hover:bg-[#2b9179] hover:text-white hover:border-white"
->
-  Hey!
-</button>
-
+      <a href="https://www.facebook.com/lorenzo.medici.3910" target="_blank" rel="noreferrer">
+        <button className="text-[#64FFDA] border-[0.5px] font-fira text-[14px] w-fit py-3 px-12 rounded-[5px] border-[#64FFDA] transition-all duration-300 hover:bg-[#2b9179] hover:text-white hover:border-white"
+                
+        >
+          Hey!
+        </button>
+      </a>
     </div>
   );
 };
